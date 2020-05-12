00aa5c12a2b13d3196416746eaad647073f00d27
sun-fox <1999sunnykumarsingh@gmail.com>
2020-05-12 10:04:53 +0530

    Basic layout formation and connections in V-DOM

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import Todos from './components/Todos'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos:[
+    {
+      id:1,
+      title:"React JS course",
+      completed:false,
+    },
+    {
+      id:2,
+      title:"Blockchain course with solidity",
+      completed:true,
+    },
+    {
+      id:3,
+      title:"ML waste categorization",
+      completed:false,
+    }]
+  }
+  render(){
+    // console.log(this.state.todos)
+    return (
+      <div className="App">
+        <h1>TodoApp</h1>
+        <Todos todos={this.state.todos}/>
+      </div>
+    );
+  }
 }
 
 export default App;
